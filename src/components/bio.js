3807147bb811b5600f5aaacb07afce0202d74eda
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-11-28 01:32:05 -0300

    edit bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -44,10 +44,11 @@ const Bio = () => {
       />
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
+          Escrito por <strong>{author.name}</strong> {author?.summary || null}
           {` `}
+          <br />
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+            Meu twitter
           </a>
         </p>
       )}
